d2245c0d0fb91fc2fbdac0880da92ef2fecfc92d
James Gomez <james.gomez320@yahoo.com>
2019-04-25 02:51:41 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,5 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
+def meal_choice(food="vegan", auto="meat")
+  puts "My favorite option is food"
+end
